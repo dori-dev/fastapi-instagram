e13efa005dce4e142e9a4a977d831e6e1b5ce1d3
dori-dev <mr.dori.dev@gmail.com>
2023-06-06 22:15:22 +0330

    add article model

--- database/models.py
+++ database/models.py
@@ -1,10 +1,25 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
 from database.db import base
 
 
 class UserModel(base):
     __tablename__ = 'users'
+
     id = Column(Integer, index=True, primary_key=True)
-    username = Column(String(63), index=True)
-    email = Column(String(127))
-    password = Column(String(63))
+    username = Column(String(50), index=True)
+    email = Column(String(100))
+    password = Column(String(50))
+    articles = relationship('ArticleModel', backref='author')
+
+
+class ArticleModel(base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, index=True, primary_key=True)
+    title = Column(String(500))
+    content = Column(String)
+    slug = Column(String(8))
+    published_at = Column(Boolean)
+    user_id = Column(Integer, ForeignKey('users.id'))
