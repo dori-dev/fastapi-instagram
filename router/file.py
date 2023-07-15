9c76326b85f919b13e76cac85cff9ad9e3656648
dori-dev <mr.dori.dev@gmail.com>
2023-07-15 16:48:35 +0330

    Add download endpoint

--- router/file.py
+++ router/file.py
@@ -1,5 +1,8 @@
+from os.path import exists as file_exists
 import shutil
-from fastapi import APIRouter, UploadFile, File
+
+from fastapi import APIRouter, UploadFile, File, status, HTTPException
+from fastapi.responses import FileResponse
 
 
 router = APIRouter(prefix='/file', tags=['file'])
@@ -16,3 +19,14 @@ def upload_file(file: UploadFile = File(...)):
         'path': path,
         'type': type,
     }
+
+
+@router.get('/download/{name}', response_class=FileResponse)
+def download_file(name: str):
+    path = f'media/{name}'
+    if file_exists(path):
+        return path
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail='This file is not exists.',
+    )
