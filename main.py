a69c5a0eee16aa7bf3dc93cbc3fa1fc8cd9b9a52
dori-dev <mr.dori.dev@gmail.com>
2023-07-17 17:15:32 +0330

    Add middleware

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from time import time
+
+from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from router import user, article, file
 from auth import authentication
@@ -15,12 +18,27 @@ app.include_router(authentication.router)
 
 app.mount('/media', StaticFiles(directory='media'), name='files')
 
+origins = [
+    'https://127.0.0.1:3000',
+    'https://127.0.0.1:8000',
+    'https://www.google.com',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware('http')
+async def add_middleware(request: Request, call_next):
+    start_time = time()
+    response: Response = await call_next(request)
+    duration = time() - start_time
+    response.headers['duration-time'] = f"{round(duration, 2)} ms"
+    return response
+
 create_all_models(base)
-
-
-# @app.exception_handler(EmailNotValid)
-# def email_not_valid(request: Request, exc: EmailNotValid):
-#     return JSONResponse(
-#         content=str(exc),
-#         status_code=status.HTTP_400_BAD_REQUEST
-#     )
